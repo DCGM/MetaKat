c472f57abd770e393f3af9dc8418b0dce54e2e0e
Jan Kohut <ikohut@fit.vutbr.cz>
2023-10-30 19:35:32 +0100

    Chapter alignment + render box.

--- datasets/parse_dataset.py
+++ datasets/parse_dataset.py
@@ -3,10 +3,13 @@ import os
 import sys
 import logging
 import time
-import Levenshtein
 
+import Levenshtein
+import cv2
+import numpy as np
 from lxml import etree
 from pero_ocr.core import layout
+from pero_ocr import sequence_alignment
 
 
 logger = logging.getLogger(__name__)
@@ -19,6 +22,7 @@ def parse_args():
     parser.add_argument('--page-xml-dir', required=True, type=str)
 
     parser.add_argument('--chapter', action='store_true')
+    parser.add_argument('--output-chapter-dir', type=str)
 
     parser.add_argument('--logging-level', default=logging.INFO)
 
@@ -42,13 +46,13 @@ def main():
     if args.chapter:
         parse_chapters(mets=args.mets,
                        page_xml_dir=args.page_xml_dir,
-                       logger=logger)
+                       output_chapter_dir=args.output_chapter_dir)
 
 
 def parse_chapters(mets: str,
                    page_xml_dir: str,
-                   max_title_transcription_relative_distance: float = 0.5,
-                   logger: logging.Logger = logging.getLogger(__name__)):
+                   max_title_transcription_relative_distance: float = 0.2,
+                   output_chapter_dir: str = None):
 
     namespaces = {'mods': "http://www.loc.gov/mods/v3",
                   'mets': "http://www.loc.gov/METS/"}
@@ -114,42 +118,178 @@ def parse_chapters(mets: str,
         page_layout = layout.PageLayout()
         page_layout.from_pagexml(page_xml_path)
 
-        mapped_text_line = None
+        transcriptions = []
+        n_char_to_n_line_mapping = []
+        for n_line, text_line in enumerate(page_layout.lines_iterator()):
+            transcriptions.append(text_line.transcription)
+            n_char_to_n_line_mapping += [n_line] * (len(text_line.transcription) + 1)
+
+        transcription = " ".join(transcriptions)
+        logger.debug(f'{"OCR PAGE TRANSCRIPTION:":>20s} {transcription}')
+        logger.debug(f'{"TITLE TO ALIGN:":>20s} {title}')
+
+        align = sequence_alignment.levenshtein_alignment_substring(
+            [x for x in transcription.lower()],
+            [x for x in title.lower()])
+        logger.debug(f'{"ALIGNMENT:":>20s} {align}')
+
+        start_align_index = crop_alignment(align)
+        end_align_index = len(align) - crop_alignment(align[::-1])
+
+        if start_align_index is None or end_align_index is None:
+            logger.debug(f'{"Alignment was not successful, MAPPING FAILED":>20s}\n')
+            continue
+
+        title_transcription = transcription[start_align_index:end_align_index]
+        title_transcription_edit_distance = Levenshtein.distance(title_transcription.lower(), title.lower())
+
+        if len(title_transcription) > len(title):
+            title_transcription_relative_similarity = (len(title_transcription) - title_transcription_edit_distance) / len(
+                title_transcription)
+        else:
+            title_transcription_relative_similarity = (len(title_transcription) + title_transcription_edit_distance) / len(
+                title_transcription)
+        align_lines = list(page_layout.lines_iterator())[n_char_to_n_line_mapping[start_align_index]:
+                                                         n_char_to_n_line_mapping[end_align_index] + 1]
+        for align_line in align_lines:
+            logger.debug(f'{"ALIGN LINES:":>20s} {align_line.transcription}')
+        logger.debug(f'{"OCR:":>20s} {title_transcription}')
+        logger.debug(f'{"METS:":>20s} {title}')
+        if 1 - max_title_transcription_relative_distance <= title_transcription_relative_similarity <= 1 + max_title_transcription_relative_distance:
+            logger.debug(f'{"MAPPED":>20s}')
+            logger.debug(f'{"ED:":>20s} {title_transcription_edit_distance}')
+            logger.debug(f'{"RS:":>20s} {title_transcription_relative_similarity}')
+            mapped_chapter_title_transcription_relative_similarity += title_transcription_relative_similarity
+            mapped_chapter_elements_count += 1
+
+            if output_chapter_dir is not None:
+                page_xml_chapter_output_path = os.path.join(output_chapter_dir, 'page_xml')
+                render_chapter_output_path = os.path.join(output_chapter_dir, 'render')
+                os.makedirs(page_xml_chapter_output_path, exist_ok=True)
+                os.makedirs(render_chapter_output_path, exist_ok=True)
+
+                max_top_height = 0
+                max_bottom_height = 0
+                left = 10000000
+                right = 0
+                top = 1000000
+                bottom = 0
+                for align_line in align_lines:
+                    if align_line.heights[0] > max_top_height:
+                        max_top_height = align_line.heights[0]
+                    if align_line.heights[1] > max_bottom_height:
+                        max_bottom_height = align_line.heights[1]
+                    if np.min(align_line.baseline[:, 0]) < left:
+                        left = align_line.baseline[0][0]
+                    if np.max(align_line.baseline[:, 0]) > right:
+                        right = align_line.baseline[-1][0]
+                    if np.min(align_line.baseline[:, 1]) < top:
+                        top = np.min(align_line.baseline[:, 1])
+                    if np.max(align_line.baseline[:, 1]) > bottom:
+                        bottom = np.max(align_line.baseline[:, 1])
+                logger.debug(f'{"MAX TOP HEIGHT":>20s} {max_top_height}')
+                logger.debug(f'{"MAX BOTTOM HEIGHT":>20s} {max_bottom_height}')
+                logger.debug(f'{"LEFT":>20s} {left}')
+                logger.debug(f'{"RIGHT":>20s} {right}')
+                logger.debug(f'{"TOP":>20s} {top}')
+                logger.debug(f'{"BOTTOM":>20s} {bottom}')
+
+                pad = 40
+                chapter_polygon = np.asarray([[left - pad, top - max_top_height - pad],
+                                             [right + pad, top - max_top_height - pad],
+                                             [right + pad, bottom + max_bottom_height + pad],
+                                             [left - pad, bottom + max_bottom_height + pad]])
+                chapter_polygon = chapter_polygon.clip(min=0)
+
+                chapter_base_name = os.path.basename(mastercopy_path)
+                chapter_base_name = os.path.splitext(chapter_base_name)[0]
+
+                chapter_page_layout = layout.PageLayout(id=chapter_base_name)
+                chapter_page_layout.regions.append(layout.RegionLayout(id=chapter_base_name,
+                                                   polygon=chapter_polygon))
+
+                chapter_page_layout.to_pagexml(os.path.join(page_xml_chapter_output_path, f'{chapter_base_name}.chapter.xml'))
+                img = cv2.imread(os.path.join("/".join(mets.split('/')[:-1]), str(mastercopy_path)))
+                img = chapter_page_layout.render_to_image(img)
+                cv2.imwrite(os.path.join(render_chapter_output_path, f'{chapter_base_name}.chapter.jpg'), img)
+
+        else:
+            logger.debug(f'{"Relative distance is too high, MAPPING FAILED":>20s}\n')
+            continue
+
+
+        # if mapped_text_lines:
+        #     logger.debug(f'{"MAPPED":>20s}')
+        #     logger.debug(f'{"OCR:":>20s} {title_transcription}')
+        #     logger.debug(f'{"METS:":>20s} {title}')
+        #     logger.debug(f'{"ED:":>20s} {title_transcription_edit_distance}')
+        #     logger.debug(f'{"RS:":>20s} {title_transcription_relative_similarity}')
+        #     mapped_chapter_title_transcription_relative_similarity += np.asarray(title_transcription_relative_similarity).mean()
+        #     mapped_chapter_elements_count += 1
+        # else:
+        #     logger.debug(f'{"MAPPING FAILED":>20s}\n')
+        #     continue
+
+        """
+        mapped_text_lines = []
+        title_transcription = ""
+        title_transcription_edit_distance = 0
+        title_transcription_relative_similarity = 0
         for text_line in page_layout.lines_iterator():
             if len(text_line.transcription.strip()) == 0:
                 continue
-            edit_distance = Levenshtein.distance(text_line.transcription, title)
-            if len(text_line.transcription) > len(title):
-                title_transcription_relative_similarity = (len(text_line.transcription) - edit_distance) / len(
-                    text_line.transcription)
+            if title_transcription == "":
+                title_transcription += text_line.transcription
             else:
-                title_transcription_relative_similarity = (len(text_line.transcription) + edit_distance) / len(
-                    text_line.transcription)
+                title_transcription += f' {text_line.transcription}'
+            title_transcription_edit_distance = sequence_alignment.levenshtein_distance_substring(
+                [x for x in title_transcription.lower()],
+                [x for x in title.lower()]
+            )
+            if len(title_transcription) > len(title):
+                title_transcription_relative_similarity = (len(title_transcription) - title_transcription_edit_distance) / len(title_transcription)
+            else:
+                title_transcription_relative_similarity = (len(title_transcription) + title_transcription_edit_distance) / len(title_transcription)
             if 1 - max_title_transcription_relative_distance <= title_transcription_relative_similarity <= 1 + max_title_transcription_relative_distance:
-                logger.debug(f'{"MAPPED":>20s}')
-                logger.debug(f'{"OCR:":>20s} {text_line.transcription}')
-                logger.debug(f'{"METS:":>20s} {title}')
-                logger.debug(f'{"ED:":>20s} {edit_distance}')
-                logger.debug(f'{"RS:":>20s} {title_transcription_relative_similarity}')
-                mapped_text_line = text_line
-                mapped_chapter_title_transcription_relative_similarity += title_transcription_relative_similarity
+                mapped_text_lines.append(text_line)
+            else:
+                mapped_text_lines = []
+                title_transcription = ""
+                title_transcription_edit_distance = 0
+                title_transcription_relative_similarity = 0
+            if len(title_transcription) > len(title):
                 break
 
-        if mapped_text_line is None:
+        if mapped_text_lines:
+            logger.debug(f'{"MAPPED":>20s}')
+            logger.debug(f'{"OCR:":>20s} {title_transcription}')
+            logger.debug(f'{"METS:":>20s} {title}')
+            logger.debug(f'{"ED:":>20s} {title_transcription_edit_distance}')
+            logger.debug(f'{"RS:":>20s} {title_transcription_relative_similarity}')
+            mapped_chapter_title_transcription_relative_similarity += np.asarray(title_transcription_relative_similarity).mean()
+            mapped_chapter_elements_count += 1
+        else:
             logger.debug(f'{"MAPPING FAILED":>20s}\n')
             continue
-        else:
-            mapped_chapter_elements_count += 1
+            
+        """
 
-        logger.debug('\n')
+        logger.debug('')
+        logger.debug('')
 
     logger.info(f'{"TOTAL MAPPED:":>20s} {mapped_chapter_elements_count}/{len(chapter_elements)}')
     if mapped_chapter_elements_count > 0:
         logger.info(f'{"ARS:":>20s} {mapped_chapter_title_transcription_relative_similarity / float(mapped_chapter_elements_count)}')
 
 
+def crop_alignment(align, dim=1):
+    for i, al in enumerate(align):
+        if al[dim] is not None:
+            return i
+
+
 def print_lxml_element(e):
-    print(etree.tostring(e, pretty_print=True))
+    logger.info(etree.tostring(e, pretty_print=True))
 
 
 if __name__ == '__main__':
