f6340ccc40235b640bb05cd8d945b2968147dcd3
Jakub Krivanek <jakubkrivanek@skirit.metacentrum.cz>
2024-01-30 13:21:21 +0100

    script for dataset preparation - chapters, page numbers, text + baseline_nn

---
+++ baseline_nn.py
@@ -0,0 +1,205 @@
+import argparse
+import os
+import json
+import numpy as np
+import torch
+import cv2
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+
+classes = ["kapitola", "cislo strany", "text"]
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dataset", required=True)
+    
+    parser.add_argument("--batch-size", type=int, default=4)
+    parser.add_argument("--epochs", type=int)
+    parser.add_argument("--model-path", required=True)
+    
+    parser.add_argument("--render-page-count", type=int)
+    parser.add_argument("--mastercopy-dir")
+    parser.add_argument("--output-render-dir")
+        
+    return parser.parse_args()
+
+class BaselineNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = nn.Conv1d(1, 6, 3)
+        self.conv2 = nn.Conv1d(6, 16, 3)
+        self.pool = nn.MaxPool1d(2, 2)
+        self.fc1 = nn.Linear(16 * 2, 120)
+        self.fc2 = nn.Linear(120, 3)
+               
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)    
+        
+        return x
+        
+def map_label(label):
+    return classes.index(label)
+    
+class JsonDataset(Dataset):
+    def __init__(self, json_file, split_ratio=0.8, train=False, test=False):
+        with open(json_file) as f:
+            self.data = json.load(f)
+            
+        train_data, test_data = train_test_split(self.data, test_size=split_ratio, shuffle=True)
+        
+        if train:
+            self.data = train_data
+        elif test:
+            self.data = test_data
+        else:
+            self.data = self.data
+            
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        input_keys = ["line_width", "line_height", "relative_line_width", "relative_line_height", "padding_top", "padding_bottom", "padding_left", "padding_right"]
+        label = torch.tensor(map_label(self.data[idx]["label"]))
+        input = torch.tensor([self.data[idx][key] for key in input_keys])
+        input = input.unsqueeze(0)
+        
+        return {"label":label, "input":input, "id":self.data[idx]["page_id"], "transcription":self.data[idx]["transcription"]}
+
+    def filter_by_page_id(self, page_id):
+        self.data = [line for line in self.data if line["page_id"] == page_id]
+
+def test_accuracy(net, tst_loader, device):
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in tst_loader:
+            inputs, labels = data["input"], data["label"]
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            outputs = net(inputs)
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    
+    if total == 0:
+        return 0    
+    return correct / total * 100
+
+def train_net(writer, net, trn_loader, tst_loader, device, epochs):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.00001, momentum=0.9)
+
+    running_loss = 0.0
+    for epoch in range(epochs):
+        
+        for i, data in enumerate(trn_loader, 0):
+            inputs, labels = data["input"], data["label"]
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            if i % 20 == 19:
+                writer.add_scalar("Loss/train", running_loss / 20, epoch * len(trn_loader) + i)
+                running_loss = 0.0
+                
+        
+        writer.add_scalar("Accuracy/test", test_accuracy(net, tst_loader, device), epoch)
+           
+
+def padding_to_bbox_pts(padding, img_width, img_height, bbox_padding=10):
+    top_padding = padding[0]
+    bottom_padding = padding[1]
+    left_padding = padding[2]
+    right_padding = padding[3]
+    top_left = [left_padding, top_padding]
+    top_right = [img_width - right_padding, top_padding]
+    bottom_right = [img_width - right_padding, img_height - bottom_padding + bbox_padding]
+    bottom_left = [left_padding, img_height - bottom_padding + bbox_padding]
+    pts = np.array([top_left, top_right, bottom_right, bottom_left], np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    return pts
+
+def render_page_bboxes(loader, page_id, mastercopy_dir, output_render_dir, net, device):
+    lines = []
+    with torch.no_grad():
+        for data in loader:
+            inputs, labels = data["input"], data["label"]
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            output = net(inputs)
+            _, data["predicted"] = torch.max(output, 1)
+            
+            lines.append(data)
+        
+    img = cv2.imread(os.path.join(mastercopy_dir, page_id + ".jpg"))
+  
+    for line in lines:
+        pts = padding_to_bbox_pts(line["input"][0][0][4:8], img.shape[1], img.shape[0])
+        
+        label = classes[line["predicted"]]
+        if label == "kapitola":
+            bbox_color = (255, 0, 0)
+        elif label == "cislo strany":
+            bbox_color = (0, 0, 255)
+        else:
+            bbox_color = (0, 255, 0)
+        img = cv2.polylines(img, [pts], True, bbox_color, 2)        
+    
+    page_id = page_id.split(".")[0]
+    cv2.imwrite(os.path.join(output_render_dir, f'{page_id}_labeled.jpg'), img)
+
+def render_dir_bboxes(mastercopy_dir, output_render_dir, render_page_count, dataset, net, device):
+    os.makedirs(output_render_dir, exist_ok=True)
+    for page_id in os.listdir(mastercopy_dir)[:render_page_count]:
+        page_id = ".".join(page_id.split(".")[:-1])
+        full_dataset = JsonDataset(dataset)
+        full_dataset.filter_by_page_id(page_id)
+        full_loader = DataLoader(full_dataset, batch_size=1, shuffle=False)
+        render_page_bboxes(full_loader, page_id, mastercopy_dir, output_render_dir, net, device)
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = BaselineNet()
+    
+    net.to(device)    
+    
+    tst_data = JsonDataset(args.dataset, test=True)    
+    tst_loader = DataLoader(tst_data, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    
+    if args.epochs:
+        trn_data = JsonDataset(args.dataset, train=True)
+        trn_loader = DataLoader(trn_data, batch_size=args.batch_size, shuffle=True, num_workers=2)   
+        
+        writer_path = os.path.join(os.path.dirname(args.model_path), "summary_writer")
+        writer = SummaryWriter(writer_path)
+             
+        train_net(writer, net, trn_loader, tst_loader, device, args.epochs)
+        torch.save(net.state_dict(), args.model_path)
+    else:
+        net.load_state_dict(torch.load(args.model_path, map_location=device))
+    
+    if args.render_page_count > 0 and args.mastercopy_dir and args.output_render_dir:
+        render_dir_bboxes(args.mastercopy_dir, args.output_render_dir, args.render_page_count, args.dataset, net, device)
+    else:
+        print(f"Test accuracy: {test_accuracy(net, tst_loader, device)}")
+            
---
+++ datasets/prepare_train_dataset.py
@@ -0,0 +1,122 @@
+import argparse
+import os
+import json
+import numpy as np
+
+from pero_ocr.core.layout import PageLayout, TextLine
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--ocr-xml-dir", required=True)
+    parser.add_argument("--labelstudio-json", required=True)
+    
+    parser.add_argument("--output-dir", required=True)
+    
+    return parser.parse_args()
+
+def baseline_in_labelbox(baseline, labelbox, lee_way=5):
+    if np.min(baseline[:, 0]) < labelbox["x"] - lee_way or np.max(baseline[:, 0]) > labelbox["x"] + labelbox["width"] + lee_way:
+        return False
+    if np.min(baseline[:, 1]) < labelbox["y"] - lee_way or np.max(baseline[:, 1]) > labelbox["y"] + labelbox["height"] + lee_way:
+        return False
+    return True
+
+def get_out_vector(line, page_layout, label=None):
+    out_vector = {}
+    if label is None:
+        out_vector["label"] = "text"
+    else:
+        out_vector["label"] = label
+        
+    page_height = page_layout.page_size[0]
+    page_width = page_layout.page_size[1]
+    
+    out_vector["page_id"] = page_layout.id    
+    out_vector["transcription"] = line.transcription
+    out_vector["line_width"] = int(np.max(line.baseline[:, 0]) - np.min(line.baseline[:, 0]))
+    out_vector["line_height"] = line.heights[0] + line.heights[1]
+    out_vector["relative_line_width"] = out_vector["line_width"] / page_width
+    out_vector["relative_line_height"] = out_vector["line_height"] / page_height
+    out_vector["padding_top"] = int(np.max(line.baseline[:, 1]) - out_vector["line_height"])
+    out_vector["padding_bottom"] = int(page_height - np.min(line.baseline[:, 1]))
+    out_vector["padding_left"] = int(np.min(line.baseline[:, 0]))
+    out_vector["padding_right"] = int(page_width - np.max(line.baseline[:, 0]))
+    return out_vector
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    os.makedirs(args.output_dir, exist_ok=True)
+    
+    with open(args.labelstudio_json) as f:
+        labestudio_json = json.load(f)
+        image_prefix = "/".join(labestudio_json[0]["image"].split("/")[0:-1])
+        image_suffix = ".jpg"
+        
+        output = []
+        
+        for page_xml_file in os.listdir(args.ocr_xml_dir):
+            
+            page_layout = PageLayout()
+            page_xml_path = os.path.join(args.ocr_xml_dir, page_xml_file)
+            page_layout.from_pagexml(page_xml_path)
+            
+            image_name = os.path.join(image_prefix, page_layout.id + image_suffix)
+            
+            obj = [x for x in labestudio_json if x["image"] == os.path.join(image_prefix, image_name)][0]
+            try:
+                labels = obj["label"]
+            except KeyError:
+                continue # skip pages without labels
+            
+            for label in labels:
+                label["x"] = label["x"] / 100 * label["original_width"]
+                label["y"] = label["y"] / 100 * label["original_height"]
+                label["width"] = label["width"] / 100 * label["original_width"]
+                label["height"] = label["height"] / 100 * label["original_height"]
+                label["value"] = label["rectanglelabels"][0]
+                
+            for line in page_layout.lines_iterator():
+                line_label = [label for label in labels if baseline_in_labelbox(line.baseline, label)]
+                if len(line_label) == 0:
+                    output.append(get_out_vector(line, page_layout))
+                else:
+                    line_label = line_label[0]["value"]
+                    if line_label not in ["kapitola", "cislo strany"]:
+                        continue
+                    output.append(get_out_vector(line, page_layout, line_label))
+        
+        text_lines_cnt = len([x for x in output if x["label"] == "text"])
+        chapter_lines_cnt = len([x for x in output if x["label"] == "kapitola"])
+        page_number_lines_cnt = len([x for x in output if x["label"] == "cislo strany"])
+        print(f"Text lines in full dataset: {text_lines_cnt}")
+        print(f"Chapter lines in full dataset: {chapter_lines_cnt}")
+        print(f"Page number lines in full dataset: {page_number_lines_cnt}")
+        
+        with open(os.path.join(args.output_dir, "dataset.json"), "w") as f:
+            json.dump(output, f)
+                     
+
+        text_lines = [x for x in output if x["label"] == "text"]
+        if len(text_lines) > len(output) / 4:
+            output = [x for x in output if x["label"] != "text"]
+            max_text_lines = int(len(output) * 3 / 4)
+            if max_text_lines < 2:
+                max_text_lines = 2
+            text_lines = np.random.choice(text_lines, max_text_lines)
+            output.extend(text_lines)
+
+        text_lines_cnt = len([x for x in output if x["label"] == "text"])
+        chapter_lines_cnt = len([x for x in output if x["label"] == "kapitola"])
+        page_number_lines_cnt = len([x for x in output if x["label"] == "cislo strany"])
+        print(f"Text lines in balanced dataset: {text_lines_cnt}")
+        print(f"Chapter lines in balanced dataset: {chapter_lines_cnt}")
+        print(f"Page number lines in balanced dataset: {page_number_lines_cnt}")
+        
+        with open(os.path.join(args.output_dir, "dataset_balanced.json"), "w") as f:
+            json.dump(output, f)
+                
+    
+    
+    
