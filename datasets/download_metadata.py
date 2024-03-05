0ff1e81ff0a2bd3b598c46376d56d013dbd77bf9
Krivoss <jakub.kriv@gmail.com>
2024-03-05 13:09:02 +0100

    title page downloading added

--- datasets/download_metadata.py
+++ datasets/download_metadata.py
@@ -3,23 +3,59 @@ import os
 import time
 import enlighten
 import argparse
+from lxml import etree
 
 manager = enlighten.get_manager()
 
+def frequency_to_number(frequency):
+    mapping = {
+        "Annual": 365,
+        "Biennial": 730,
+        "Bimonthly": 60,
+        "Biweekly": 14,
+        "Daily": 1,
+        "denně": 1,
+        "Monthly": 30,
+        "Quarterly": 90,
+        "Semimonthly": 15,
+        "Semiweekly": 3,
+        "Three times a month": 10,
+        "Triennial": 1095,
+        "Unknown": 0,
+        "Weekly": 7
+    }
+    
+    try:
+        return mapping[frequency]
+    except KeyError:
+        print(f"Unknown frequency: {frequency}, returning 0")
+        return 0
 
-def get_response(url):
+
+def get_response(url, output_dir=None):
     retry_times = 5
     for _ in range(retry_times):
         try:
             response = requests.get(url)
         except requests.exceptions.ConnectTimeout:
-            time.sleep(10)
+            time.sleep(5)
             continue
         if response.status_code == 200 or response.status_code == 404:
             break
-        time.sleep(2)
+        time.sleep(1)
     if response.status_code != 200:
-        print(f"Response status code: {response.status_code} after {retry_times} retries, URL: {url}, skipping...")
+        print(f"Response status code: {response.status_code}, URL: {url}, skipping...")
+    if response.status_code == 404 and output_dir is not None:
+        structure_file = os.path.join("..", output_dir, "structure.txt")
+        uuid = url.split("uuid:")[1].split("/")[0]
+        with open(structure_file, "r") as f:
+            lines = f.readlines()
+        with open(structure_file, "w") as f:
+            for line in lines:
+                if uuid in line:
+                    f.write(f"{uuid} 404: Not Found\n")
+                else:
+                    f.write(line)
     return response
 
 
@@ -38,7 +74,9 @@ def parse_args():
     parser.add_argument("--doc-model", type=str, help="Filter only lines from --file with this model")
     parser.add_argument("--output-dir", type=str, required=True, help="Output directory")
 
-    parser.add_argument("--pages", action="store_true", help="Download pages metadata")
+    parser.add_argument("--pages", action="store_true", help="Download pages metadata", default=False)
+    parser.add_argument("--title-pages", action="store_true", help="Download only title pages metadata", default=False)
+    parser.add_argument("--max-freq", type=int, help="Minimum frequency of periodicals to download", default=0)
 
     return parser.parse_args()
 
@@ -47,6 +85,7 @@ class Periodical:
     def __init__(self, uuid):
         self.uuid = uuid
         self.years = {}
+        self.frequency = 0
 
         self.years_count = 0
         self.issues_count = 0
@@ -97,22 +136,38 @@ class KrameriusAPI:
 
         periodical = Periodical(uuid)
         self.download_periodical_metadata(periodical, output_dir)
+        
+        if args.max_freq != 0 and (periodical.frequency > args.max_freq or periodical.frequency == 0):
+            print(f"Frequency of periodical is {periodical.frequency}, not continuing downloading...")
+            return
+            
         print("Metadata for periodical downloaded")
         self.download_periodical_years_metadata(periodical, output_dir)
         print(f"Metadata for years downloaded ({periodical.get_years_count()} total)")
         self.download_periodical_issues_metadata(periodical, output_dir)
         print(f"Metadata for issues downloaded ({periodical.get_issues_count()} total)")
-        if args.pages:
-            self.download_periodical_pages_metadata(periodical, output_dir)
-            print(f"Metadata for pages downloaded ({periodical.get_pages_count()} total)")
+        if args.pages or args.title_pages:
+            self.download_periodical_pages_metadata(periodical, output_dir, title_pages_only=args.title_pages)
+            print(f"Metadata for title pages downloaded ({periodical.get_pages_count()} total)")
 
     def download_periodical_metadata(self, periodical, output_dir):
         url = f"{self.base_url}/search/api/client/{self.version}/items/uuid:{periodical.uuid}/metadata/mods"
-        response = get_response(url)
+        response = get_response(url, output_dir)
         if response.status_code != 200:
             return
         with open(f"{output_dir}/uuid:{periodical.uuid}.xml", "w") as f:
             f.write(response.text)
+        
+        frequency = None
+        namespaces = {"mods": "http://www.loc.gov/mods/v3"}
+        tree = etree.fromstring(response.text.encode())
+        for f in tree.iterfind(".//mods:frequency", namespaces):
+            if f.get("authority") != "marcfrequency":
+                frequency = f
+                break
+        if frequency is not None:
+            periodical.frequency = frequency_to_number(frequency.text)
+        
 
     def download_periodical_years_metadata(self, periodical, output_dir):
         year_uuids = self.get_children_uuids(periodical.uuid, os.path.join(output_dir, "structure.txt"))
@@ -126,7 +181,7 @@ class KrameriusAPI:
             if os.path.exists(year_path):
                 print(f"Year metadata for {year_uuid} already exists, skipping downloading...")
                 continue
-            response = get_response(url)
+            response = get_response(url, output_dir)
             if response.status_code != 200:
                 continue
             with open(year_path, "w") as f:
@@ -145,18 +200,42 @@ class KrameriusAPI:
                 if os.path.exists(issue_path):
                     print(f"Issue metadata for {issue_uuid} already exists, skipping downloading...")
                     continue
-                response = get_response(url)
+                response = get_response(url, output_dir)
                 if response.status_code != 200:
                     continue
                 with open(issue_path, "w") as f:
                     f.write(response.text)
 
-    def download_periodical_pages_metadata(self, periodical, output_dir):
-        for year_uuid in periodical.get_years():
-            for issue_uuid in periodical.get_issues(year_uuid):
-                page_uuids = self.get_children_uuids(issue_uuid, os.path.join(output_dir, year_uuid, issue_uuid, "structure.txt"))
+    def download_periodical_pages_metadata(self, periodical, output_dir, title_pages_only=False):
+        periodical_years = periodical.get_years()
+        for year_num, year_uuid in enumerate(periodical_years):
+            if len(periodical_years) > 5:
+                if year_num % (len(periodical_years) // 4) != 0:
+                    continue
+                
+            periodical_issues = periodical.get_issues(year_uuid)
+            for issuen_num, issue_uuid in enumerate(periodical_issues):
+                if len(periodical_issues) > 5:
+                    if issuen_num % (len(periodical_issues) // 4) != 0:
+                        continue
+                
+                issue_dir = os.path.join(output_dir, year_uuid, issue_uuid)
+                page_uuids = self.get_children_uuids(issue_uuid, os.path.join(issue_dir, "structure.txt"))
 
-                for page_uuid in page_uuids:
+                
+                for page_num, page_uuid in enumerate(page_uuids):                    
+                    title_page_exists = False
+                    if title_pages_only:
+                        for dirpath, _, filenames in os.walk(issue_dir):
+                            for filename in filenames:
+                                if "_title.xml" in filename:
+                                    print(f"Title page for issue {issue_uuid} already exists, skipping downloading...")
+                                    title_page_exists = True
+                    
+                    if title_page_exists:
+                        break                
+                        
+                    end_searching = False
                     periodical.add_page(year_uuid, issue_uuid, page_uuid)
                     url = f"{self.base_url}/search/api/client/{self.version}/items/uuid:{page_uuid}/metadata/mods"
 
@@ -166,16 +245,35 @@ class KrameriusAPI:
                     if os.path.exists(page_path):
                         print(f"Page metadata for {page_uuid} already exists, skipping downloading...")
                     else:
-                        response = get_response(url)
+                        response = get_response(url, output_dir)
                         if response.status_code != 200:
                             continue
+                        
+                        namespaces = {"mods": "http://www.loc.gov/mods/v3"}
+                        tree = etree.fromstring(response.text.encode())
+                        if tree.find(".//mods:part[@type='TitlePage']", namespaces) is not None:
+                            page_path = os.path.join(page_dir, f"uuid:{page_uuid}_title.xml")
+                            if title_pages_only:
+                                end_searching = True
+                        elif title_pages_only and page_num > 10:
+                            end_searching = True
+                            print(f"Title page not found withing first 10 pages, ending search for this issue...")
+
                         with open(page_path, "w") as f:
                             f.write(response.text)
+                        
+                        if end_searching:
+                            break
+
 
     def get_children_uuids(self, uuid, structure_file):
         if os.path.exists(structure_file):
             with open(structure_file, "r") as f:
-                return [line.strip() for line in f]
+                lines = []
+                for line in f:
+                    if "404: Not Found" in line:
+                        continue
+                    lines.append(line.strip())
 
         url = f"{self.base_url}/search/api/client/{self.version}/items/uuid:{uuid}/info/structure"
         response = get_response(url)
