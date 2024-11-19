b69c753de401347fe5c7c66b24bb7ba20a989f2b
Marie Pařilová <parilovam@email.cz>
2024-11-19 23:17:53 +0100

    update

--- metakat/biblio/parser.py
+++ metakat/biblio/parser.py
@@ -6,6 +6,7 @@ from lxml import etree
 import xml.etree.ElementTree as ET
 import ftfy
 import re
+from rapidfuzz import fuzz
 
 # Funkce pro opravu textu
 def fix_text(text):
@@ -40,45 +41,84 @@ def parse_mods(file_path):
     date_issued = root.find(".//mods:originInfo/mods:dateIssued", ns)
     data['date_issued'] = fix_text(date_issued.text) if date_issued is not None else "N/A"
 
+    # Místo vydání
+    place = root.find(".//mods:originInfo/mods:place/mods:placeTerm", ns)
+    data['place_of_publication'] = fix_text(place.text) if place is not None else None
+
+    # Nakladatel
+    publisher = root.find(".//mods:originInfo/mods:publisher", ns)
+    data['publisher'] = fix_text(publisher.text) if publisher is not None else None
+
+    # Rok vydání
+    date_issued = root.find(".//mods:originInfo/mods:dateIssued", ns)
+    data['year_of_publication'] = fix_text(date_issued.text) if date_issued is not None else None
+
+    # Jazyk
+    language = root.find(".//mods:language/mods:languageTerm[@type='code']", ns)
+    data['language'] = fix_text(language.text) if language is not None else None
+    
+    # Fyzický popis
+    physical_description = []
+    for extent in root.findall(".//mods:physicalDescription/mods:extent", ns):
+        physical_description.append(fix_text(extent.text))
+    data['physical_description'] = physical_description
+
+    # Žánr
+    genre = root.find(".//mods:genre", ns)
+    data['genre'] = fix_text(genre.text) if genre is not None else None
+
+    # Identifikátory
+    identifiers = {}
+    for identifier in root.findall(".//mods:identifier", ns):
+        id_type = identifier.get('type')
+        id_value = fix_text(identifier.text)
+        identifiers[id_type] = id_value
+    data['identifiers'] = identifiers
+
     return data
 
 
 # Funkce pro přiřazení kategorií textům
-def assign_category(text):
+def assign_category(text, data):
     categories = {
-        'titulek': ['uhlonošne útvary', 'tasmánii', 'název', 'headline', 'title'],
-        'cislo': ['číslo', 'ii', 'iii', 'iv', 'issue'],
+        'titulek': ['title'],
         'rocnik': ['ročník', 'number'],
-        'datum cisla': ['datum', 'issue date', 'date issued'],
-        'misto vydani': ['vydání', 'place of publication', 'place of print'],
-        'nakladatel': ['nakladatelství', 'publisher'],
-        'podtitulek': ['podtitulek', 'subtitle'],
-        'redaktor': ['redaktor', 'editor', 'writing', 'compiler'],
-        'vydavatel': ['vydavatel', 'publisher'],
-        'datum rocniku': ['datum ročníku', 'year of edition'],
-        'autor': ['autor', 'feistmantel', 'author', 'written by'],
-        'prekladatel': ['překladatel', 'translator'],
-        'ilustrator': ['ilustrátor', 'illustrator'],
-        'vydani': ['vydání', 'edition'],
-        'dil': ['díl', 'volume', 'part'],
-        'nazev dilu': ['název dílu', 'title of part'],
-        'cislo serie': ['číslo série', 'series number'],
-        'datum vydani': ['datum vydání', 'publication date'],
+        'misto vydani': ['place_of_publication'],
+        'nakladatel': ['publisher'],
+        'podtitulek': ['subtitle'],
+        'vydavatel': ['publisher'],
+        'autor': ['authors'],
+        'prekladatel': ['translator'],
+        'vydani': ['edition'],
+        'dil': ['volume', 'part'],
+        'nazev dilu': ['title of part'],
+        'datum vydani': ['date_issued'],
         'serie': ['serie', 'series'],
         'editor': ['editor'],
-        'tiskar': ['tiskař', 'printer'],
-        'misto tisku': ['místo tisku', 'place of print']
+        'tiskar': ['printer'],
+        'misto tisku': ['place of print']
     }
     
-    # Text pro porovnání
-    normalized_text = normalize_text(text)
+    best_match = None
+    highest_score = 0
+    threshold=70
     
-    # Procházení kategorií
-    for category, keywords in categories.items():
-        if any(keyword in normalized_text for keyword in keywords):
-            return category
-    
-    return 'unknown'
+    for category, keys in categories.items():
+        for key in keys:
+            if key in data:
+                value = data[key]
+                if isinstance(value, list):  # Pokud je hodnota seznam
+                    value = " ".join(value)  # Spojí hodnoty seznamu do jednoho řetězce
+
+                # Vypočítej podobnost mezi textem a hodnotou
+                score = fuzz.partial_ratio(text.lower(), value.lower())
+                
+                # Pokud skóre překročí práh a je nejlepší, ulož kategorii
+                if score > threshold and score > highest_score:
+                    highest_score = score
+                    best_match = category
+
+    return best_match if best_match else "unknown"
 
 # Funkce pro převod PAGE XML na JSON pro Label Studio
 def parse_page_xml_to_json(xml_path, mods_path, output_dir):
@@ -89,13 +129,8 @@ def parse_page_xml_to_json(xml_path, mods_path, output_dir):
     image_height = int(page.attrib["imageHeight"])
 
     data = parse_mods(mods_path)
-    title = data.get('title', 'N/A')
-    author = data.get('authors', ['N/A'])[0]  # Pokud více autorů, vezmeme první
-    date_issued = data.get('date_issued', 'N/A')
 
-    print(f"Title: {title}")
-    print(f"Author: {author}")
-    print(f"Date Issued: {date_issued}")
+    print(data)
 
     annotations = []
 
@@ -116,7 +151,7 @@ def parse_page_xml_to_json(xml_path, mods_path, output_dir):
         text_equiv = text_region.find(".//{http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15}Unicode")
         text = text_equiv.text if text_equiv is not None else ""
 
-        category = assign_category(text)
+        category = assign_category(text, data)
 
         # pokud kategorie není 'unknown'
         if category != "unknown":
@@ -136,10 +171,7 @@ def parse_page_xml_to_json(xml_path, mods_path, output_dir):
     # Výstupní JSON
     result = {
         "data": {
-            "image": image_filename,
-            "title": title,
-            "author": author,
-            "date_issued": date_issued
+            "image": image_filename+'.jpg'
         },
         "annotations": [
             {
