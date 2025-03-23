83080d0e5717081f0f322e2f18a630d0f9e7afd9
Matěj Šmida <momo4cz@gmail.com>
2025-03-23 17:51:18 +0100

    Clip Model with classification head

--- metakat/page_type/Clip/ClipModel.py
+++ metakat/page_type/Clip/ClipModel.py
@@ -1,22 +1,19 @@
 import torch
+import os
 from torch import nn
 from transformers import CLIPModel, CLIPConfig
 from transformers.utils import ModelOutput
 from transformers.modeling_outputs import BaseModelOutputWithPooling
-from typing import Optional, Union, Tuple
+from typing import Optional, Union, Tuple, Any
 
-"""
-class CLIPOutput(ModelOutput):
-    
+
+class CLIPWithClassificationHeadOutput(ModelOutput):
+    """
     Args:
         loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `return_loss` is `True`):
             Contrastive loss for image-text similarity.
-        logits_per_image (`torch.FloatTensor` of shape `(image_batch_size, text_batch_size)`):
-            The scaled dot product scores between `image_embeds` and `text_embeds`. This represents the image-text
-            similarity scores.
-        logits_per_text (`torch.FloatTensor` of shape `(text_batch_size, image_batch_size)`):
-            The scaled dot product scores between `text_embeds` and `image_embeds`. This represents the text-image
-            similarity scores.
+        logits (`torch.FloatTensor` of shape `(batch_size, num_labels)`)
+            Logits of combined image and text features
         text_embeds (`torch.FloatTensor` of shape `(batch_size, output_dim`):
             The text embeddings obtained by applying the projection layer to the pooled output of [`CLIPTextModel`].
         image_embeds (`torch.FloatTensor` of shape `(batch_size, output_dim`):
@@ -25,11 +22,10 @@ class CLIPOutput(ModelOutput):
             The output of the [`CLIPTextModel`].
         vision_model_output (`BaseModelOutputWithPooling`):
             The output of the [`CLIPVisionModel`].
-    
+    """
 
     loss: Optional[torch.FloatTensor] = None
-    logits_per_image: torch.FloatTensor = None
-    logits_per_text: torch.FloatTensor = None
+    logits: torch.FloatTensor = None
     text_embeds: torch.FloatTensor = None
     image_embeds: torch.FloatTensor = None
     text_model_output: BaseModelOutputWithPooling = None
@@ -40,33 +36,15 @@ class CLIPOutput(ModelOutput):
             self[k] if k not in ["text_model_output", "vision_model_output"] else getattr(self, k).to_tuple()
             for k in self.keys()
         )
-"""
+
 class ClipWithClassificationHead(CLIPModel):
-    def __init__(self, config: CLIPConfig, num_classes: int):
+    def __init__(self, config: CLIPConfig, num_classes: int = None):
         super().__init__(config)
 
-        text_config = config.text_config
-        vision_config = config.vision_config
+        self.classificationHead = None
+        self.init_classification_head()
 
-        # text_dim = text_config.hidden_size
-        # vision_dim = vision_config.hidden_size
-
-        text_dim = text_config.projection_dim
-        vision_dim = vision_config.projection_dim
-
-        print(f"TEXT DIM: {text_dim} \nVISION DIM: {vision_dim}")
-
-        #TODO add changeable parameters (from config?)
-        self.classificationHead = nn.Sequential(
-            nn.Linear(text_dim + vision_dim, 512),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(512, num_classes),
-        )
-
-        #TODO weight init
-
-        # print(nn.Linear.weight)
+        self.post_init()
 
     def forward(
             self,
@@ -108,9 +86,14 @@ class ClipWithClassificationHead(CLIPModel):
         text_embeds = text_outputs[1]
         text_embeds = self.text_projection(text_embeds)
 
+        """
+        Used for one image and and more text inputs
+        Might use this later
+        
         batch_size = text_embeds.size(0)
         image_embeds = image_embeds.unsqueeze(0).expand(batch_size, -1, -1).squeeze(1)
         print(f"EMBDEDS {image_embeds.size()}")
+        """
 
         combined_features = torch.cat((image_embeds, text_embeds), dim=-1)
 
@@ -121,27 +104,46 @@ class ClipWithClassificationHead(CLIPModel):
         print("LOGITS")
         print(logits.size())
         print("LABELS")
-        print(labels.size())
+        print(labels.size)
 
         loss = None
         if return_loss:
-            if input_ids is not None:
-                #TODO change loss function based on config?
-                loss_fcn = nn.CrossEntropyLoss()
-                loss = loss_fcn(logits, labels)
-        """
-        if not return_dict:
-            return (loss, logits) if loss is not None else logits
+            loss_fcn = nn.CrossEntropyLoss()
+            loss = loss_fcn(logits, labels)
 
-        
-        return BaseModelOutputWithPooling(
+        if not return_dict:
+            output = (logits, text_embeds, image_embeds, text_outputs, vision_outputs)
+            return ((loss,) + output) if loss is not None else output
+
+        return CLIPWithClassificationHeadOutput(
             loss=loss,
             logits=logits,
             text_embeds=text_embeds,
             image_embeds=image_embeds,
-            text_outputs=text_outputs,
-            vision_outputs=vision_outputs,
+            text_model_output=text_outputs,
+            vision_model_output=vision_outputs,
         )
-        """
-        return (loss, logits) if loss is not None else logits
 
+    def init_classification_head(self):
+        text_dim = self.config.text_config.projection_dim
+        vision_dim = self.config.vision_config.projection_dim
+
+        num_classes = self.config.num_classes
+
+        self.classificationHead = nn.Sequential(
+            nn.Linear(text_dim + vision_dim, 512),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(512, num_classes),
+        )
+
+    @classmethod
+    def from_pretrained(
+            cls,
+            pretrained_model_name_or_path: Optional[Union[str, os.PathLike]],
+            **kwargs,
+    ):
+        model = super().from_pretrained(pretrained_model_name_or_path, **kwargs)
+        model.init_classification_head()
+
+        return model
