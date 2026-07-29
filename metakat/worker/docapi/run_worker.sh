e5c498fbeaec115e36b1777945c9566f1d3637b0
Jan Kohut <ikohut@fit.vutbr.cz>
2026-07-29 09:29:08 +0200

    Update worker bash script.

--- metakat/worker/docapi/run_worker.sh
+++ metakat/worker/docapi/run_worker.sh
@@ -1,5 +1,7 @@
-export WORKER_KEY=metakat.pNUDguoq.5ojmR7qVU207ttkMZF3mznsYHlLWDYDUgnt9ac62
-export BASE_DIR=/home/ikohut/data/metakat_worker
+export WORKER_KEY=
+export BASE_DIR=/mnt/kolosus/data/metakat_worker
+export ENGINES_DIR=/home/ikohut/data/metakat_worker/engines
+export LOGGING_DIR=/home/ikohut/data/metakat_worker/logs
 
 export PYTHONPATH=/home/ikohut/Projects/MetaKat/libs/DocAPI:/home/ikohut/Projects/MetaKat/libs/detector-wrapper:/home/ikohut/Projects/MetaKat
 
